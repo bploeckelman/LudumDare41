b1342147e4c8409d5c2a105056a87592b3784e9a
Brian Ploeckelman <brian.ploeckelman@gmail.com>
2018-04-21 13:41:04 -0500

    Add temp tileset for test map

---
+++ core/assets/maps/tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="tileset" tilewidth="32" tileheight="32" spacing="4" margin="2" tilecount="196" columns="14">
+ <image source="tileset.png" width="512" height="512"/>
+</tileset>
