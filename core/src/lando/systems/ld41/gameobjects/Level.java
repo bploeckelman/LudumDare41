e3e8ee063dc33fcc04eae12cedc6a79308f95fcc
Doug Graham <doug.graham@wisc.edu>
2018-04-21 18:12:15 -0500

    Allow for more than one polyline in the map

--- core/src/lando/systems/ld41/gameobjects/Level.java
+++ core/src/lando/systems/ld41/gameobjects/Level.java
@@ -33,7 +33,7 @@ public class Level {
     public TiledMapTileLayer wallsLayer;
     public MapLayer objectsLayer;
 
-    Polyline boundary;
+    Array<PolylineMapObject> boundaries;
     Array<EllipseMapObject> circles;
     Vector2 tempVector;
     Vector2 tempVector2;
@@ -66,9 +66,8 @@ public class Level {
         // TODO: load map objects
 
         // load collision polygons
-        Array<PolylineMapObject> bounds = collisionLayer.getObjects().getByType(PolylineMapObject.class);
-        if (bounds.size < 1) throw new GdxRuntimeException("Forgot to add boundary layer to the map");
-        boundary = bounds.get(0).getPolyline();
+        boundaries = collisionLayer.getObjects().getByType(PolylineMapObject.class);
+        if (boundaries.size < 1) throw new GdxRuntimeException("Forgot to add boundary layer to the map");
 
         circles = collisionLayer.getObjects().getByType(EllipseMapObject.class);
     }
@@ -87,12 +86,16 @@ public class Level {
             shapes.setColor(Color.RED);
             shapes.setProjectionMatrix(LudumDare41.game.screen.worldCamera.combined);
             {
-                int vertLength = boundary.getVertices().length;
-                for (int i = 0; i < vertLength; i += 2) {
-                    tempVector.set(boundary.getTransformedVertices()[i], boundary.getTransformedVertices()[i + 1]);
-                    tempVector2.set(boundary.getTransformedVertices()[(i + 2) % vertLength],
-                                    boundary.getTransformedVertices()[(i + 3) % vertLength]);
-                    shapes.line(tempVector.x, tempVector.y, tempVector2.x, tempVector2.y);
+                for (int j=0; j < boundaries.size; j++) {
+                    Polyline boundary = boundaries.get(j).getPolyline();
+
+                    int vertLength = boundary.getVertices().length;
+                    for (int i = 0; i < vertLength; i += 2) {
+                        tempVector.set(boundary.getTransformedVertices()[i], boundary.getTransformedVertices()[i + 1]);
+                        tempVector2.set(boundary.getTransformedVertices()[(i + 2) % vertLength],
+                                boundary.getTransformedVertices()[(i + 3) % vertLength]);
+                        shapes.line(tempVector.x, tempVector.y, tempVector2.x, tempVector2.y);
+                    }
                 }
 
                 for (int i = 0; i < circles.size; i++){
@@ -115,32 +118,36 @@ public class Level {
      * @return if there was an intersection
      */
     public boolean checkCollision(Vector2 oldPosition, Vector2 newPosition, float radius, Vector2 collisionPoint, Vector2 normal){
-        int vertLength = boundary.getVertices().length;
-        for (int i = 0; i < vertLength; i+= 2){
-            tempVector.set(boundary.getTransformedVertices()[i], boundary.getTransformedVertices()[i+1]);
-            tempVector2.set(boundary.getTransformedVertices()[(i+2)%vertLength], boundary.getTransformedVertices()[(i+3)%vertLength]);
+        for (int j=0; j < boundaries.size; j++) {
+            Polyline boundary = boundaries.get(j).getPolyline();
 
-            // Check if the traveling path intersects the segment
-            if (Intersector.intersectSegments(oldPosition, newPosition, tempVector, tempVector2, collisionPoint)){
-                normal.set((tempVector2.y - tempVector.y), -1 * (tempVector2.x - tempVector.x));
-                normal.nor();
-                collisionPoint.add(normal.scl(radius));
-                normal.nor();
+            int vertLength = boundary.getVertices().length;
+            for (int i = 0; i < vertLength; i += 2) {
+                tempVector.set(boundary.getTransformedVertices()[i], boundary.getTransformedVertices()[i + 1]);
+                tempVector2.set(boundary.getTransformedVertices()[(i + 2) % vertLength], boundary.getTransformedVertices()[(i + 3) % vertLength]);
 
-                return true;
+                // Check if the traveling path intersects the segment
+                if (Intersector.intersectSegments(oldPosition, newPosition, tempVector, tempVector2, collisionPoint)) {
+                    normal.set((tempVector2.y - tempVector.y), -1 * (tempVector2.x - tempVector.x));
+                    normal.nor();
+                    collisionPoint.add(normal.scl(radius));
+                    normal.nor();
+
+                    return true;
+                }
+
+                // Check if the segments are within the radius of the object
+                if (Intersector.distanceSegmentPoint(tempVector, tempVector2, newPosition) < radius) {
+                    Intersector.nearestSegmentPoint(tempVector, tempVector2, newPosition, collisionPoint);
+                    normal.set((tempVector2.y - tempVector.y), -1 * (tempVector2.x - tempVector.x));
+                    normal.nor();
+                    collisionPoint.add(normal.scl(radius));
+                    normal.nor();
+
+                    return true;
+                }
+
             }
-
-            // Check if the segments are within the radius of the object
-            if (Intersector.distanceSegmentPoint(tempVector, tempVector2, newPosition) < radius){
-                Intersector.nearestSegmentPoint(tempVector, tempVector2, newPosition, collisionPoint);
-                normal.set((tempVector2.y - tempVector.y),-1* (tempVector2.x - tempVector.x));
-                normal.nor();
-                collisionPoint.add(normal.scl(radius));
-                normal.nor();
-
-                return true;
-            }
-
         }
 
         for (int i = 0; i < circles.size; i++){
